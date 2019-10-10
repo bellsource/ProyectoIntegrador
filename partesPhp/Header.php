537eb38d85548783206882af83177d9e30435d1c
Camilo Franco <53304018+k1000oooooo@users.noreply.github.com>
2019-10-10 20:50:41 -0300

    usuario.json / registro.php / partes
    En partes esta el nav y el footer (productos todavia no lo hicimos porque aun no es obligatorio)

---
+++ partesPhp/Header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Libros de Coolto</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link href="https://fonts.googleapis.com/css?family=Barlow&amp;display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="img/Logo.png">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light col-lg-12">
+    <a class="logo col-md-2 col-lg-1" href="index.php">
+    <div class="logo col-md-2 col-lg-1">
+      <img src="img/Logo.png" width="80%">
+    </div>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+          <a class="nav-link" href="registro.html">Libros</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="registro.html">Registro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="login.html">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="preguntasFrecuentes.html">Preguntas Frecuentes</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+      </form>
+    </div>
+  </nav>
+</body>
---
+++ registrointe.php
@@ -0,0 +1,145 @@
+
+<?php
+
+session_start();
+if(isset($_COOKIE["inputNombre"])){
+      $_SESSION["inputNombre"] = $_COOKIE["inputNombre"];
+      header("Location:index.php");
+  }
+  
+  
+  
+  
+  
+  //inicializo variables para errores
+  $errorNombre = "";
+  $errorEmail = "";
+  $errorPassword = "";
+  $errorAvatar = "";
+  
+  //averiguo si enviaron el formulario
+  if($_POST){
+    $errores = false;
+    
+    //valido los datos
+    if($_POST["inputNombre"] == ""){
+      $errorNombre = "Ingrese un nombre";
+      $errores = true;
+    }else if(strlen($_POST["inputNombre"]) < 2){
+        $errorNombre = "Su nombre debe tener al menos 2 caracteres";
+        $errores = true;
+    }
+    //aca va la validación del apellido
+    if($_POST["inputPass"] == "" || $_POST["inputPassConfirm"] == ""){
+        $errorPassword = "No puede estar en blanco";
+        $errores = true;
+    }else if($_POST["inputPass"] != $_POST["inputPassConfirm"]){
+        $errorPassword = "Las contraseñas deben coincidir";
+        $errores = true;
+    }else{
+        $contrasenia = password_hash($_POST["inputPass"],PASSWORD_DEFAULT);
+        //echo md5($_POST["password"]);
+         } 
+
+    
+
+    if(!$errores){
+        //no hay errores
+
+        //creo el usuario
+        $usuario = [
+            "id"=> md5($_POST["inputNombre"]),
+            "username" => $_POST["inputNombre"],
+            "email" => $_POST["inputEmail"],
+            "password" => $contrasenia
+        ];
+        
+        //traigo los usuarios del json
+        $usuariosEnJSON = file_get_contents("usuarios.json");
+        //convierto el json en array
+        $usuarios = json_decode($usuariosEnJSON);
+        //agrego el nuevo usuario al array de la base de datos
+        $usuarios[] = $usuario;
+        //convierto el nuevo array completo a json
+        $nuevosUsuariosEnJSON = json_encode($usuarios);
+        //escribo el nuevo json en el archivo .json
+        file_put_contents("usuarios.json",$nuevosUsuariosEnJSON);
+
+        header("Location:index.php");
+        exit;
+            }
+
+        }
+
+        if($_POST){
+                if($_POST["recordarme"] != null){
+                  setCookie("inputNombre",$_POST["inputNombre"]);
+              }
+              $_SESSION["inputNombre"] = $_POST["inputNombre"];
+              header("Location:index.php");
+            }
+ ?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>Registro</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+
+
+     <form class="formcontainer" method="POST" enctype="multipart/form-data">
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="inputNombre">Nombre</label>
+          <input type="text" name="inputNombre" class="form-control" id="inputNombre" placeholder="Nombre">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="inputApellido">Apellido</label>
+          <input type="text" name="inputApellido" class="form-control" id="inputApellido" placeholder="Apellido">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="inputEmail">Email</label>
+          <input type="email" name="inputEmail" class="form-control" id="inputEmail" placeholder="Email">
+        </div>
+
+        <div class="form-group col-md-6">
+          <label for="inputPass">Contraseña</label>
+          <input type="password" name="inputPass" class="form-control" id="inputPass">
+        </div>
+        <div class="form-group col-md-12">
+          <label for="inputPassConfirm">Confirmar Contraseña</label>
+          <input type="password" name="inputPassConfirm" class="form-control">
+        </div>
+        <div class="form-group col-md-12">
+          <label for="inputLocalidad">Localidad</label>
+          <select id="inputLocalidad" name="inputLocalidad" class="form-control">
+            <option selected>Elegir...</option>
+            <option>Argentina</option>
+            <option>Mexico</option>
+            <option>Chile</option>
+            <option>Ecuador</option>
+            <option>Perú</option>
+          </select>
+        </div>
+        <div class="form-group col-md-12">
+          <label for="inputPassConfirm">Recordarme</label>
+          <input type="checkbox" value="recordar" name="recordarme">
+        </div>
+
+      </div>
+
+      <button type="submit" class="formbutton">Crear Cuenta</button>
+    </form>
+
+
+
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
