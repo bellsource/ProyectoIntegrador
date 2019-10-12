5822c05847d0590426b11bc0e111f827408500e7
Camilo Franco <53304018+k1000oooooo@users.noreply.github.com>
2019-10-12 11:40:41 -0300

    Subo con los ultimos cambios que hice
    Subo con los ultimos cambios que hice

--- partesPhp/Header.php
+++ partesPhp/Header.php
@@ -12,10 +12,12 @@
 </head>
 
 <body>
+  <!--  col-xs-1 col-sm-1 col-md-2 col-lg-1 -->
+  <!--  width="40px" -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light col-lg-12">
-    <a class="conteiner fluid" href="index.php">
-    <div class="logo col-xs-12 col-sm-12 col-md-2 col-lg-1">
-      <img src="img/Logo.png" width="80%">
+    <a class="conteiner fluid"  href="index.php">
+    <div class="logo">
+      <img src="img/Logo.png" class= "img-responsive" width="40px">
     </div>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -42,4 +44,8 @@
       </form>
     </div>
   </nav>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  
 </body>
