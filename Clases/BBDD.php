9264f52e244e51ce08a2f3e0078e084a1efd9b83
Camilo Franco <53304018+k1000oooooo@users.noreply.github.com>
2019-11-06 17:19:58 -0300

    Clases
    Estoy subiendo la carpeta con dos clases armadas: BBDD y usuarios. armé la función para traer los suarios (llamando esa funcion deberíamos poder traer los datos en un formulario y poder editarlos. public function traerUsuario($id){
            $consulta = $this->conexion->prepare("SELECT * FROM usuarios");
            $consulta->execute();
            return $consulta->fetch(PDO::FETCH_ASSOC);
        Faltaria hacer que la función que edite los datos de la tabla, que haga un update.

---
+++ Clases/BBDD.php
@@ -0,0 +1,95 @@
+<?php
+class BBDD{
+
+    private $user;
+    private $pass;
+    private $db_name;
+    private $host;
+    private $conexion;
+
+    public function __construct($db_name,$user = "root",$pass = "root",$host = "localhost"){
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->db_name = $db_name;
+        $this->host = $host;
+
+        $opciones = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        );
+
+        try{
+            $dsn = 'mysql:host='.$this->host.';dbname='.$this->db_name;
+            $this->conexion = new PDO($dsn, $this->user, $this->pass, $opciones);
+        }catch(PDOException $Exception){
+            echo $Exception->getMessage();
+        }
+    }
+    
+    public function getUser() {
+        return $this->user;
+    }
+
+    public function  setuser($user) {
+        return $this->user;
+    }
+    
+    public function getPass() {
+        return $this->pass;
+    }
+
+    public function  setPass($pass) {
+        return $this->pass;
+    }
+
+    public function getDb_name() {
+        return $this->db_name;
+    }
+
+    public function  setDb_name($db_name) {
+        return $this->db_name
+    }
+
+    public function getHost() {
+        return $this->host;
+    }
+
+    public function  setHost($host) {
+        return $this->host;
+    }
+
+    public function getConexion() {
+        return $this->conexion;
+    }
+
+    public function  setConexion($conexion) {
+        return $this->conexion;
+    }
+
+    public function traerUsuario($id){
+        $consulta = $this->conexion->prepare("SELECT * FROM usuarios");
+        $consulta->execute();
+        return $consulta->fetch(PDO::FETCH_ASSOC);
+    
+
+    public function traerLibros(){
+        $consulta = $this->conexion->query("SELECT * FROM Libros");
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function traerGeneros(){
+        $consulta = $this->conexion->query("SELECT * FROM genres");
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function traerLibrosPorGenero($genero_id){
+        $consulta = $this->conexion->prepare("SELECT * FROM Libros WHERE genero_id = :genero_id");
+        $consulta->bindValue(":genero_id",$genero_id);
+        $consulta->execute();
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function editarGenero($id,$nuevoName){
+
+    }
+
+}
---
+++ Clases/usuarios.php
@@ -0,0 +1,63 @@
+<?php
+
+class Usuarios {
+    private $nombre;
+    private $apellido;
+    private $email;
+    private $pass;
+    private $foto;
+    private $admin;
+
+    public function __construct ($nombre, $mail, $pass, $admin) {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->email = $email;
+        $this->pass = $pass;
+        $this->foto = $foto;
+        $this->admin= $admin
+  }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function  setNombre($nombre) {
+        return $this->nombre;
+    }
+
+    public function  getApellido() {
+        return $this->apellido;
+    }
+    public function  setApellido($apellido) {
+        return $this->apellido;
+    }
+
+    public function  getEmail() {
+        return $this->email;
+    }
+    public function  setEmail($email) {
+        return $this->email;
+    }
+
+    public function  getPass() {
+        return $this->pass;
+    }
+    public function  setPass($pass) {
+        return $this->pass;
+    }
+
+    public function  getFoto() {
+        return $this->foto;
+    }
+    public function setFoto($foto) {
+        return $this->foto;
+    }
+
+    public function  getAdmin() {
+        return $this->admin;
+    }
+    public function setAdmin($admin) {
+        return $this->admin;
+    }
+
+}
